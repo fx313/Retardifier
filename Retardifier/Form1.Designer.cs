08c62c9db976ac474f403ab63d4ca6ed4807f84e
Felix Anselm <felix.anselm@zalando.de>
2021-05-21 21:46:40 +0200

    Projektdateien hinzufügen.

---
+++ Retardifier/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Retardifier
+{
+    partial class Retardifier
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(93, 111);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(460, 23);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(93, 140);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(460, 23);
+            this.textBoxOutput.TabIndex = 1;
+            this.textBoxOutput.TextChanged += new System.EventHandler(this.textBoxOutput_TextChanged);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelTitle.Location = new System.Drawing.Point(12, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(208, 86);
+            this.labelTitle.TabIndex = 2;
+            this.labelTitle.Text = "label1";
+            this.labelTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.No;
+            this.trackBar1.Location = new System.Drawing.Point(12, 249);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(662, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Retardifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(686, 306);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Retardifier";
+            this.Text = "Retardifier";
+            this.TransparencyKey = System.Drawing.Color.DarkRed;
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Retardifier/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Retardifier
+{
+    public partial class Retardifier : Form
+    {
+        public Retardifier()
+        {
+            InitializeComponent();
+            labelTitle.Text = "R E T A R D I F I E R";
+            timer1.Enabled = true;
+        }
+
+        private string retardThis(string input)
+        {
+            string stringOutput = "";
+            //char charToCheck;
+            char[] charArray = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    charArray[i] = Char.ToLower(input[i]);
+                }
+                else
+                {
+                    charArray[i] = Char.ToUpper(input[i]);
+                }
+            }
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                stringOutput = stringOutput + charArray[i];
+            }
+
+            
+            return stringOutput;
+        }
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = retardThis(textBoxInput.Text);
+        }
+
+        private void textBoxOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Random rand = new Random();
+            int A = rand.Next(0, 255);
+            int R = rand.Next(0, 255);
+            int G = rand.Next(0, 255);
+            int B = rand.Next(0, 255);
+
+            labelTitle.ForeColor = Color.FromArgb(A, R, G, B);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int value;
+
+            value = trackBar1.Maximum - trackBar1.Value;
+            if (value < 1)
+            {
+                value = 1;
+            }
+            timer1.Interval = value;
+
+        }
+    }
+}
